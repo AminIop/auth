d7576892541ba028fea8c6bf46732ce287f44e2f
amine <amine.tamouh.officiel@gmail.com>
2024-04-03 22:28:19 +0000

    add actions-types

---
+++ src/store/constants/actions-types/counter.js
@@ -0,0 +1,12 @@
+
+export const CounterType = {
+    BYONE: {
+        GET: {
+            START: 'counter.byOne.get:start',
+            SUCCESS: 'counter.ByOne.get:success',
+            FAIL: 'counter.ByOne.get:fail',
+        },
+        INCREMENT: 'counter.byOne:increment',
+        DECREMENT: 'counter.byOne:decrement',
+    }
+}
